89bc79a6bba81b7bff05873ab16445f9dce6621e
griff182uk <griff182uk@yahoo.co.uk>
2025-03-29 18:27:59 +0000

    feat: :sparkles: integrate authentication middleware and create auth routes file

--- fastapi_course/employee_repo/main.py
+++ fastapi_course/employee_repo/main.py
@@ -3,11 +3,14 @@ from fastapi import FastAPI
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
 from .routes import employee_routes
+from .auth import auth_middleware
 
 app = FastAPI()
 
 app.mount("/static", StaticFiles(directory="./employee_repo/static"), name="static")
 
 templates = Jinja2Templates(directory="./employee_repo/templates")
+
+app.middleware("http")(auth_middleware)
 
 app.include_router(employee_routes.router)
